421620bc512105dffbcfca1c25e01a0f7f8077e4
Eliokay <150727302+Eliokay@users.noreply.github.com>
2024-03-17 16:25:31 -0700

    first commit

---
+++ Query3.php
@@ -0,0 +1,39 @@
+<?php
+$servername = 'cssql.seattleu.edu';
+$username = 'll_eokoloko';
+$password = 'Cpsc2300Eokoloko';
+$dbname = 'll_eokoloko';
+
+//Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+//check connection
+if (!$conn){
+    die('connection filed: ' . mysqli_connect_error());
+}
+
+$sql = "SELECT * FROM ParticipantInformation
+WHERE ParticipantID IN (
+  SELECT ParticipantID 
+  FROM ParticipantConsent
+  WHERE Response = 'Yes'
+);";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+echo "<table border = '1'>\n";
+// output data of each row
+while($row = mysqli_fetch_row($result)) {
+echo "<tr>\n";
+for ($i = 0; $i < mysqli_num_fields($result); $i++) {
+echo "<td>" . $row[$i] . "</td>\n";
+}
+echo "</tr>\n";
+}
+echo "</table>\n";
+} else {
+echo "0 results";
+}
+mysqli_free_result($result);
+mysqli_close($conn);
+?>
---
+++ Query4.php
@@ -0,0 +1,42 @@
+<?php
+$servername = 'cssql.seattleu.edu';
+$username = 'll_eokoloko';
+$password = 'Cpsc2300Eokoloko';
+$dbname = 'll_eokoloko';
+
+//Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+//check connection
+if (!$conn){
+    die('connection filed: ' . mysqli_connect_error());
+}
+
+$sql = "SELECT 
+s.StudyName, 
+COUNT(pt.TestID) AS TotalTests
+FROM Study s
+LEFT JOIN ParticipantStudy ps ON s.StudyID = ps.StudyID  
+LEFT JOIN ParticipantTest pt ON ps.ParticipantID = pt.ParticipantID
+GROUP BY s.StudyID
+HAVING TotalTests <> 0;";
+
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+echo "<table border = '1'>\n";
+// output data of each row
+while($row = mysqli_fetch_row($result)) {
+echo "<tr>\n";
+for ($i = 0; $i < mysqli_num_fields($result); $i++) {
+echo "<td>" . $row[$i] . "</td>\n";
+}
+echo "</tr>\n";
+}
+echo "</table>\n";
+} else {
+echo "0 results";
+}
+mysqli_free_result($result);
+mysqli_close($conn);
+?>
